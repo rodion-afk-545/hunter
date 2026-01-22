249b626b0d89dff71cc18ae6f409877d6921dde1
rodion-afk-545 <kalinrodion546@gmail.com>
2026-01-22 19:05:07 +0300

    обновление стрельбы

--- Assets/Scripts/MoveBullet.cs
+++ Assets/Scripts/MoveBullet.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class MoveBullet : MonoBehaviour
 {
-   private float speed = 4;
+   private float speed = 15;
 
     
     void Update()
     {
         
-        transform.Translate(Vector3.down * Time.deltaTime * speed);
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
     }
 }
